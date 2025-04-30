a06baf3e9556835d9f33ce07ebbab85a67584421
Arul Mathur <arulster17@gmail.com>
2025-04-30 05:00:52 -0700

    added logic for non-array inputs

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -25,9 +25,9 @@ function sumValues(num1, num2, add) {
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
+    if (!Array.isArray(prices) || prices.length == 0) return false;
     const discounted = [];
     const length = prices.length;
-    if (length == 0) return false;
     let discountedPrice = 0;
     for(let i = 0; i < length; i++) {
         discountedPrice = prices[i] * (1 - discount);
@@ -36,4 +36,5 @@ function discountPrices(prices, discount) {
 
     return discounted;
 }
+
 module.exports = {sumValues, discountPrices};
